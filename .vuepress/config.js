a835b3d4bba9f2e57561ae0f3d9f2e17a43b7b48
Sylvain Pace <sylvain.pace@algolia.com>
2020-04-11 11:25:36 +0200

    feat: add DocSearch as recommended by vuepress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
   title: 'FeathersJS',
   description: 'A REST and real-time API layer for modern applications',
   themeConfig: {
+    algolia: {
+      apiKey: 'feathersjs',
+      indexName: 'feathersjs'
+    },
     logo: '/img/feathers-logo-wide.png',
     repo: 'feathersjs/feathers',
     docsRepo: 'feathersjs/docs',
